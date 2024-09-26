1d662a96de56d4eb40f44184000cb63c1c689ffd
ryzhovalex <thed4rkof@gmail.com>
2024-09-26 17:54:53 +0300

    update stat.gd, distance_sv.gd, distance_sv.tscn, ...

--- mob.gd
+++ mob.gd
@@ -1,17 +1,9 @@
 extends Area2D
+class_name Mob
 signal hit
 
-@export var spd = 5
-@export var max_hp = 100
-@export var hp = max_hp
-@export var dmg = 10
-@export var atk_spd = 2
-
-var screen_size
-
-func _ready():
-    screen_size = get_viewport_rect().size
+@export var stat: MobStat
 
 func _process(delta):
     $AnimatedSprite2D.play()
-    position += Vector2(1, 1) * spd * delta
+    position += Vector2(1, 1) * stat.move_spd * delta
---
+++ mob_stat.gd
@@ -0,0 +1,10 @@
+extends Stat
+class_name MobStat
+
+@export var move_spd = 5
+@export var max_hp = 100
+@export var hp = max_hp
+@export var dmg = 10
+@export var atk_cooldown_ms = 3000
+@export var dmg_recover_ms = 3000
+@export var atk_range = 100
