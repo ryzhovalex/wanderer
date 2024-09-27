d0bad82ffcc03d7cee1d072f029f47e8657c7f0a
ryzhovalex <thed4rkof@gmail.com>
2024-09-27 18:05:22 +0300

    update mob.gd, mob_stat.gd, spawner_sv.tscn

--- mob/mob.gd
+++ mob/mob.gd
@@ -8,14 +8,20 @@ signal hit
 enum State {
     Idle,
     Move,
-    Atk,
+    StartedAtk,
+    EndedAtk,
     Die
 }
 var _state := State.Idle
 var last_atk_time_ms: int = 0
+var is_moving_backward: bool = false
 
 func _ready():
     _sprite.connect("animation_finished", _on_anim_finished)
+    assert(
+        stat.closest_range <= stat.atk_range,
+        "Idle range must be less than attack range"
+    )
 
 func _process(delta):
     if _state == State.Die:
@@ -25,16 +31,27 @@ func _process(delta):
     if stat.hp <= 0:
         _die()
         return
-    if _is_in_atk_range():
+
+    if _state == State.Idle || _state == State.Move:
+        var distance := position.distance_to(_player_char.position)
+        is_moving_backward = distance < stat.closest_range
+
+    # Flip sprite to face the character
+    if is_moving_backward:
+        _sprite.flip_h = _player_char.position.x > position.x
+    else:
+        _sprite.flip_h = _player_char.position.x <= position.x
+
+    if _is_in_atk_range() && !is_moving_backward:
         # Wait&Stare if atk is recharging, but player within the range
-        if _state != State.Atk && _state != State.Idle && _is_atk_on_cooldown():
+        if _state != State.Idle && _state != State.StartedAtk && _is_atk_on_cooldown():
             print_debug("idle")
             _state = State.Idle
             _sprite.play("idle")
             return
         _atk()
         return
-    _move_toward(delta)
+    _move(delta)
 
 func _die():
     if _state == State.Die:
@@ -48,10 +65,10 @@ func _is_atk_on_cooldown():
 func _on_anim_finished():
     print_debug("finish anim while %d" % _state)
     match _state:
-        State.Atk:
+        State.StartedAtk:
             if _is_in_atk_range():
                 _send_dmg()
-            _state = State.Idle
+            _state = State.EndedAtk
         State.Die:
             queue_free()
             return
@@ -64,18 +81,30 @@ func _is_in_atk_range() -> bool:
     return stat.atk_range >= distance
 
 func _atk():
-    if _state == State.Atk || _is_atk_on_cooldown():
+    if _state == State.StartedAtk || _is_atk_on_cooldown():
         return
     print_debug("atk")
     # Register an attack once animation starts, not on animation finish
     last_atk_time_ms = core.current_ms()
-    _state = State.Atk
+    _state = State.StartedAtk
     _sprite.play("atk")
 
-func _move_toward(delta: float):
+func _move_backward_player_char(delta: float):
+    position = position.move_toward(
+        _player_char.position, stat.move_spd * delta * -1
+    )
+
+func _move_toward_player_char(delta: float):
+    position = position.move_toward(
+        _player_char.position,
+        stat.move_spd * delta
+    )
+
+func _move(delta: float):
     if _state != State.Move:
         _state = State.Move
         _sprite.play("move")
-    # Flip sprite to face the character
-    _sprite.flip_h = _player_char.position.x < position.x
-    position = position.move_toward(_player_char.position, stat.move_spd * delta)
+    if is_moving_backward:
+        _move_backward_player_char(delta)
+        return
+    _move_toward_player_char(delta)
--- mob/mob_stat.gd
+++ mob/mob_stat.gd
@@ -9,4 +9,9 @@ class_name MobStat
 
 @export var atk_dmg: int = 10
 @export var atk_range: int = 100
+# How close the mob can be to the target. If the distance between mob and
+# target is less than this value, the mob will try to make such distance on
+# first possibility. If the distance is greater than this value, but less than
+# `atk_range`, the mob will idle.
+@export var closest_range: int = 50
 @export var atk_cooldown_ms: int = 3000
