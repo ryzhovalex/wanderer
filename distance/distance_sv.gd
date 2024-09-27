c475320eb2244e8c3106f1888e7a755dd032ab2f
ryzhovalex <thed4rkof@gmail.com>
2024-09-27 15:47:33 +0300

    update distance_sv.gd, main.tscn, player_char.gd

--- distance/distance_sv.gd
+++ distance/distance_sv.gd
@@ -1,6 +1,11 @@
 extends Sv
 
+# How many pixels in 1 meter
+@export var meter_to_pixels: float = 1
+
 @onready var player_char: PlayerChar = core.current("PlayerChar")
+@onready var covered_distance_text: Label = core.current("CoveredDistanceText")
+
 var player_char_starting_x: int
 # Distance are not distracted when moved backwards, but added only once
 var covered_distance: int
@@ -9,9 +14,17 @@ func _ready():
     player_char_starting_x = floor(player_char.position.x)
     print("Player character is starting from x %d" % player_char_starting_x)
 
-func _get_covered_distance() -> int:
+func _physics_process(_delta):
+    covered_distance = _process_covered_distance()
+    covered_distance_text.text = \
+        "%d meters" % _get_covered_distance_as_meters()
+
+func _process_covered_distance() -> int:
     var new_covered_distance: int = \
         floor(player_char.position.x) - player_char_starting_x
     if new_covered_distance < covered_distance:
         new_covered_distance = covered_distance
     return new_covered_distance
+
+func _get_covered_distance_as_meters() -> int:
+    return floor(covered_distance / meter_to_pixels)
--- player/player_char.gd
+++ player/player_char.gd
@@ -4,6 +4,11 @@ class_name PlayerChar
 func _ready():
     $AnimatedSprite2D.speed_scale = 0.35
     $AnimatedSprite2D.play("idle")
+    # Get back to the ground dude
+    position.y = GroundSv.base_y
+
+func _draw():
+    draw_line(Vector2(position.x, GroundSv.base_y), Vector2(5000, GroundSv.base_y), Color.GREEN, 1.0)
 
 func _process(delta):
     position.x += delta
